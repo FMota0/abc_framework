36d96cda65a87b7d681c83c026305b84c135bc68
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2021-04-29 15:38:34 -0300

    Add tooltip to strategy selector

---
+++ client/src/components/StrategySelector/Tooltip.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { selectedStrategyTitle } from "../../constants";
+import { getResearches } from "../../store/programs/selectors";
+
+function Tooltip({
+  programId,
+  strategy,
+  position,
+}) {
+  const researches = useSelector(getResearches(programId, strategy));
+
+  const containerStyle = {
+    position: "absolute",
+    top: position.y + 10,
+    left: position.x + 10,
+    backgroundColor: "black",
+    color: "white",
+    padding: 5,
+  };
+  return (
+    <div style={containerStyle}>
+      <div>{selectedStrategyTitle[strategy]}</div>
+      {researches.map(research => (
+        <div key={`tooltip-research-${research._id}`} style={{ fontSize: 12, }}>{research.title}</div>
+      ))}
+    </div>
+  )
+};
+
+export default Tooltip;
