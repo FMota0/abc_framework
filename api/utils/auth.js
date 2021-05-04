49f437f59cb097b3ea46dd9295ff1f5d747c7ea2
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2021-05-03 21:51:20 -0300

    Fix identation

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -3,18 +3,18 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_ID)
 
 module.exports = async (token) => {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.GOOGLE_AUDIENCE,
-    })
-    const {
-        name,
-        email,
-        picture,
-    } = ticket.getPayload()
-    return {
-        name,
-        email,
-        picture,
-    }
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_AUDIENCE,
+  })
+  const {
+    name,
+    email,
+    picture,
+  } = ticket.getPayload()
+  return {
+    name,
+    email,
+    picture,
+  }
 }
